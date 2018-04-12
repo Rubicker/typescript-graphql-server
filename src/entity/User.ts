2dc43d321cfb81634a7715c761305dc52c6a42f6
Rubicker <newsl00001@gmail.com>
2018-04-12 21:24:46 +0800

    :sparkles: Add CURD with Typeorm

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,13 +1,19 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
 @Entity()
-export class User {
+export class User extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column({ type: "boolean", default: false })
+  @Column({ type: "text", unique: true })
+  email: string;
+
+  @Column({ type: "bool", default: false })
   confirmed: boolean;
 
-  @Column() lastName: string;
+  @Column({ type: "varchar", length: "230" })
+  firstName: string;
 
-  @Column() age: number;
+  @Column("text") lastName: string;
+
+  @Column("text") age: number;
 }
